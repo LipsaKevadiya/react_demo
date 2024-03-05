4318353031bce75555253d48c09637edcdb0c583
lipsa <lipsa.kevadiya@exhibytesolution.com>
2024-03-05 18:31:47 +0530

    Created react app called my-app and understood the structure of React js project File and Folder and used Functional Component of React.

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Simple from "./FuncCompo.js";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <Simple />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/FuncCompo.js
@@ -0,0 +1,9 @@
+function Simple() {
+  return (
+    <div>
+      <h1>welcome here</h1>
+    </div>
+  );
+}
+
+export default Simple;
